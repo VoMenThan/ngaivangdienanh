2eb889623c92b599c14a79688d43da8091f08f41
VoMenThan <thancn12@gmail.com>
2017-04-21 08:35:50 +0700

    no message

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<section>
+    <acticle>
+        <div class="logo-nvda"><img src="images/logo.png" alt="logo ngai vàng điện ảnh"></div>
+        <div class="slider-nvda">
+            <img src="images/banner_01.jpg" alt="">
+        </div>
+    </acticle>
+    <acticle>
+        <nav>
+            <ul class="menu-top">
+                <li class="menu-detail">
+                    <a href="">Giới thiệu</a>
+                </li>
+                <li class="menu-detail">
+                    <a href="">Thể lệ</a>
+                </li>
+                <li class="menu-detail">
+                    <a href="">Tin tức</a>
+                </li>
+                <li class="menu-detail">
+                    <a href="">Hình ảnh</a>
+                </li>
+                <li class="menu-detail">
+                    <a href="">Video</a>
+                </li>
+            </ul>
+        </nav>
+    </acticle>
+</section>
+</body>
+</html>
