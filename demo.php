9bb04837b19f95bc22bacde0332d2b6da7ed4025
VoMenThan <thancn12@gmail.com>
2017-04-23 21:16:30 +0700

    update

--- demo.php
+++ demo.php
@@ -1,64 +1,21 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-
-<div class="slideshow-container">
-
-    <div class="mySlides fade">
-        <img src="img_nature_wide.jpg" style="width:100%">
-    </div>
-
-    <div class="mySlides fade">
-        <img src="img_fjords_wide.jpg" style="width:100%">
-    </div>
-
-    <div class="mySlides fade">
-        <img src="img_mountains_wide.jpg" style="width:100%">
-    </div>
-
-
-
-</div>
-<br>
-
-<div style="text-align:center">
-    <span class="dot" onclick="currentSlide(1)"></span>
-    <span class="dot" onclick="currentSlide(2)"></span>
-    <span class="dot" onclick="currentSlide(3)"></span>
-</div>
-
-<script>
-    var slideIndex = 1;
-    showSlides(slideIndex);
-
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
-
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex-1].style.display = "block";
-        dots[slideIndex-1].className += " active";
-    }
-</script>
-
+<section>
+    <article>
+        <div class="div red"></div>
+    </article>
+    <article>
+        <div class="div"></div>
+    </article>
+</section>
 </body>
 </html>
---
+++ thele.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Intro</title>
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <link href="css/video-js.css" rel="stylesheet">
+    <script src="js/videojs-ie8.min.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<header>
+    <div class="intro-video">
+        <video poster="images/poster.png" id="my-video" class="video-js" controls preload="auto" width="100%" height="264"
+               data-setup="{}">
+            <source src="video/yeu5.mp4" type='video/mp4'>
+            <source src="video/yeu5.webm" type='video/webm'>
+            <p class="vjs-no-js">
+                To view this video please enable JavaScript, and consider upgrading to a web browser that
+                <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+            </p>
+        </video>
+    </div>
+    <nav>
+        <ul class="menu-top">
+            <li class="menu-detail">
+                <a href="">Giới thiệu</a>
+            </li>
+            <li class="menu-detail">
+                <a class="active" href="">Thể lệ</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Tin tức</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Hình ảnh</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Video</a>
+            </li>
+        </ul>
+    </nav>
+    <article class="title-head">
+        <div class="row-bar"></div>
+        <div class="label-head">Thí Sinh</div>
+        <h1>Thể Lệ Cuộc Thi</h1>
+        </div>
+    </article>
+</header>
+<section>
+    <article>
+        <div class="content">
+            <h3>A. Đối tượng tham dự</h3>
+            <p>
+                Nam Nữ công dân Việt Nam, hiện đang sinh sống tại Việt Nam hoặc nước ngoài.
+                Từ 16 tuổi trở lên.
+                Không chịu sự quản lý độc quyền bởi các công ty/đơn vị/tổ chức trong và ngoài nước (tính đến thời điểm dự thi).
+
+            </p>
+            <h3>B. Thời gian dự thi</h3>
+            <p>
+                -Vòng Casting Online (15/4/2017 - 1/6/2017)
+                Vòng Casting trực tiếp (10/6/201 7 tại TPHCM và 1 7/6/2017 tại Hà Nội)
+                Vòng loại trực tiếp (1 5,1 6/7/201 7)
+                + Học lớp học diễn xuất + Học Tiếng Nói Sân Khấu.
+                + Học Hình thể.
+                + Diễn xuất trước ống kính.
+                + Tham quan 1 số đoàn phim và các hoạt động xã hội.
+                *Sau các buổi học, tất cả thí sinh sẽ phải làm 1 phim ngắn để thể hiện mình ( đây là một trong những giải nằm trong khuôn khổ của cuộc thi).
+                -Vòng Bán kết (Tháng 11/2017 tại TPHCM)
+                -Vòng chung kết (Tháng 12/2017 tại Nha Trang)
+            </p>
+            <h3>C. Cơ cấu giải thưởng</h3>
+            <p>
+                Giải Ngai Vàng Gương mặt sân khấu điện ảnh triển vọng 2017
+                000 VNĐ
+                Giải Nhất: Hoa hậu điện ảnh, Nam đế điện ảnh
+                000 VNĐ
+                02 Giải Nhì: Á Hậu 1 điện ảnh, Á Vương 1 điện ảnh
+                000 VNĐ
+                02 Giải Ba: Á Hậu 2 điện ảnh, Á Vương 2 điện ảnh
+                000 VNĐ
+                Các giải thưởng phụ, mỗi giải thưởng trị giá 10.000.000 VNĐ
+                Giải diễn xuất hay nhất
+                Giải gương mặt ăn ảnh nhất
+                Giải thí sinh được yêu thích nhất
+                Giải báo chí bình chọn
+                Giải hình thể độc đáo
+                <span class="des">
+                    * Còn những giải phụ của các nhà tài trợ nếu có, sẽ được thông báo trong vòng bán kết .
+                </span>
+            </p>
+        </div>
+        <div class="hashtag">
+            <div class="box-social">
+                <a class="icon-social" href="tag-facebook"><img class="fb" src="images/connect-on.png" alt="facebook"></a>
+                <a class="icon-social" href="tag-share"><img class="sh" src="images/connect-on.png" alt="share"></a>
+                <a class="icon-social" href="tag-youtube"><img class="you" src="images/connect-on.png" alt="youtube"></a>
+            </div>
+            <div class="tag-name">
+                Tag: <span>#NGAI</span> <span>#VANG</span> <span>#DIEN</span> <span>#ANH</span> <span>#2017</span>
+            </div>
+        </div>
+    </article>
+</section>
+<footer>
+    <div class="box-footer">
+        <h3>Ban Tổ Chức</h3>
+        <img class="img-logo-organize" src="images/cty-sieu-nhan-viet.png" alt="">
+        <img class="img-logo-organize" src="images/cty-mouse.png" alt="">
+    </div>
+    <div class="box-footer logo-footer">
+        <img src="images/logo.png" alt="logo ngai vàng điện ảnh">
+    </div>
+    <div class="box-footer contact ">
+        <div class="contact-footer">
+            Liên hệ BTC <br>
+            0909 000000
+        </div>
+        <div class="contact-footer">
+            Liên hệ quảng áo <br>
+            Công ty Dihaan Media <br>
+            0909 000000
+        </div>
+    </div>
+    <div class="donors"><span> Nhà tài trợ</span> <img src="images/Donors.png" alt="Nhà tài trợ"></div>
+    <div class="copyright">
+        <div class="copy">All right reserved Dehaan Media.</div>
+        <div class="copy">Giấy phép số BCT/123445/78</div>
+    </div>
+</footer>
+<script src="js/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script src="js/slick.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/video.js"></script>
+<script type="text/javascript">
+    $(document).on('ready', function() {
+        $(".regular").slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            speed: 500
+        });
+    });
+</script>
+</body>
+</html>
