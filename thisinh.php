12d17761673b1955a84133d4fc4bbc97a27845b8
vomenthan <thancn12@gmail.com>
2017-04-24 17:47:27 +0700

    no message

---
+++ thisinh.php
@@ -0,0 +1,151 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Intro</title>
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <link href="css/video-js.css" rel="stylesheet">
+    <script src="js/videojs-ie8.min.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<header>
+    <div class="intro-video">
+        <video poster="images/poster.png" id="my-video"
+               class="video-js"
+                controls
+                width="100%" height="264"
+                data-setup='{ "techOrder": ["youtube", "html5"], "sources": [{ "type": "video/youtube", "src": "https://www.youtube.com/watch?v=aajlaOzL2-Q"}] }'
+        >
+            <p class="vjs-no-js">
+                To view this video please enable JavaScript, and consider upgrading to a web browser that
+            </p>
+        </video>
+    </div>
+    <div id="sticky-anchor-scroll"></div>
+    <nav class="nav-top">
+        <ul class="menu-top-scroll">
+            <li class="menu-detail btn-close"><span class="close-x">X</span></li>
+            <li class="menu-detail">
+                <a href="">Giới thiệu</a>
+            </li>
+            <li class="menu-detail">
+                <a class="active" href="">Thể lệ</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Tin tức</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Hình ảnh</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Video</a>
+            </li>
+        </ul>
+        <img class="btn-menu" src="images/btn-menu.png" alt="menu">
+    </nav>
+    <article class="title-head">
+        <div class="row-bar"></div>
+        <div class="label-head">Thí Sinh</div>
+        <h1>Thể Lệ Cuộc Thi</h1>
+        </div>
+    </article>
+</header>
+<section>
+    <article>
+        <div class="content">
+            <p>
+                Nhằm tìm kiếm những tài năng cho nền nghệ thuật thứ 7, trải qua 5 mùa thi chương trình "Gương mặt sân khấu điện ảnh triển vọng"đã phát hiện và cung cấp cho nền sân khấu - điện ảnh những gương mặt sáng giá. Mùa thứ 6, với sự kết hợp của Công ty Siêu Nhân Việt và Công ty Dihaan Media sẽ đưa cuộc thi "Gương mặt sân khấu - điện ảnh triển vọng 2017" mang tính chuyên nghiệp cao, nơi thí sinh thể hiện sự trải nghiệm sân khấu - điện ảnh, tài năng cá nhân, sựbùng cháy đam mê.
+            </p>
+            <p>
+                Cuộc thi "Gương mặt sân khấu điện ảnh triển vọng" đã trải qua sự chuyển mình qua rất nhiều giai đoạn từ các cuộc thi, chương trình đã đào tạo và tìm kiếm ra nhiều gương mặt thành danh từ đây như Ngân Khánh, Minh Luân, Lương Thế Thành, Khương Ngọc...v..v...
+            </p>
+            <p>
+                Đến năm 201 3, chương trình đã chính thức được Sở Văn Hóa TPHCM phê duyệt là bản quyền chính thức thuộc sở hữu của công tyTNHH Dịch vụ Siêu Nhân Việt.
+            </p>
+        </div>
+        <div class="hashtag">
+            <div class="box-social">
+                <a class="icon-social" href="tag-facebook"><img class="fb" src="images/connect-on.png" alt="facebook"></a>
+                <a class="icon-social" href="tag-share"><img class="sh" src="images/connect-on.png" alt="share"></a>
+                <a class="icon-social" href="tag-youtube"><img class="you" src="images/connect-on.png" alt="youtube"></a>
+            </div>
+            <div class="tag-name">
+                Tag: <span>#NGAI</span> <span>#VANG</span> <span>#DIEN</span> <span>#ANH</span> <span>#2017</span>
+            </div>
+        </div>
+    </article>
+</section>
+<footer>
+    <div class="box-footer">
+        <h3>Ban Tổ Chức</h3>
+        <img class="img-logo-organize" src="images/cty-sieu-nhan-viet.png" alt="">
+        <img class="img-logo-organize" src="images/cty-mouse.png" alt="">
+    </div>
+    <div class="box-footer logo-footer">
+        <img src="images/logo.png" alt="logo ngai vàng điện ảnh">
+    </div>
+    <div class="box-footer contact ">
+        <div class="contact-footer">
+            Liên hệ BTC <br>
+            0909 000000
+        </div>
+        <div class="contact-footer">
+            Liên hệ quảng áo <br>
+            Công ty Dihaan Media <br>
+            0909 000000
+        </div>
+    </div>
+    <div class="donors"><span> Nhà tài trợ</span> <img src="images/icon-cat.png" alt="Nhà tài trợ"></div>
+    <div class="copyright">
+        <div class="copy">All right reserved Dehaan Media.</div>
+        <div class="copy">Giấy phép số BCT/123445/78</div>
+    </div>
+</footer>
+<script src="js/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script src="js/slick.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/video.js"></script>
+<script src="js/youtube.js"></script>
+<script type="text/javascript">
+    $(document).on('ready', function() {
+        $(".regular").slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            speed: 500
+        });
+        /*js menu top mobile*/
+        $('.btn-menu').click(function(event){
+            event.stopPropagation();
+            $('.menu-top-scroll').fadeIn();
+        });
+        $('.close-x').click(function () {
+            $('.menu-top-scroll').fadeOut();
+        })
+        /*js nav scroll top*/
+        function sticky_relocate() {
+            var window_top = $(window).scrollTop();
+            var div_top = $('#sticky-anchor-scroll').offset().top;
+            if (window_top > div_top) {
+                $('.nav-top').addClass('stick-scroll');
+                $('#sticky-anchor-scroll').height($('.nav-top').outerHeight());
+            } else {
+                $('.nav-top').removeClass('stick-scroll');
+                $('#sticky-anchor-scroll').height(0);
+            }
+        }
+        $(function() {
+            $(window).scroll(sticky_relocate);
+            sticky_relocate();
+        });
+    });
+</script>
+</body>
+</html>
