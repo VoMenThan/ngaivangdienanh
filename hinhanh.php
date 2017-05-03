a57f7da477ec601d499dc2e902ff03b3473c4822
VoMenThan <thancn12@gmail.com>
2017-05-03 08:26:38 +0700

    no message

---
+++ hinhanh.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tin tuc</title>
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <link href="css/video-js.css" rel="stylesheet">
+    <script src="js/videojs-ie8.min.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<header>
+    <div class="intro-video">
+        <video poster="images/poster.png" id="my-video"
+               class="video-js"
+                controls
+                width="100%" height="264"
+                data-setup='{ "techOrder": ["youtube", "html5"], "sources": [{ "type": "video/youtube", "src": "https://www.youtube.com/watch?v=aajlaOzL2-Q"}] }'
+        >
+            <p class="vjs-no-js">
+                To view this video please enable JavaScript, and consider upgrading to a web browser that
+            </p>
+        </video>
+        <img src="images/logo.png" alt="logo" class="logo-video-nvda">
+    </div>
+    <div id="sticky-anchor-scroll"></div>
+    <nav class="nav-top nav-home">
+        <a href="http://ngaivangdienanh.com">
+            <img src="images/logo.png" alt="logo" class="logo-nav-nvda">
+        </a>
+        <ul class="menu-top-scroll">
+            <li class="menu-detail btn-close"><span class="close-x">X</span></li>
+            <li class="menu-detail">
+                <a href="">Giới thiệu</a>
+            </li>
+            <li class="menu-detail">
+                <a class="active" href="">Thể lệ</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Tin tức</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Hình ảnh</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Video</a>
+            </li>
+        </ul>
+        <img class="btn-menu" src="images/btn-menu.png" alt="menu">
+    </nav>
+    <article class="title-head">
+        <div class="row-bar"></div>
+        <div class="label-head">Thí Sinh</div>
+        <div class="box-title-news"></div>
+    </article>
+</header>
+<section class="news-section">
+    <div class="title-content-news">Tin tức</div>
+    <article class="box-content-news">
+        <div class="news-box">
+            <a class="title-news-content" href="#">Title</a>
+            <a href="#"><img class="img-news" src="images/news.jpg" alt=""></a>
+            <div class="content-right">
+                Một số cử tri đề nghị thành phố tập trung giải quyết những vấn đề liên quan đến ranh giới đất đai giữa địa phương với quốc phòng.
+
+                Tiếp thu ý kiến, Bí thư Hà Nội cho rằng, đây là lĩnh vực mà những việc nhỏ không được giải quyết nhanh và tích cực thì có thể nảy sinh thành vấn đề lớn.
+            </div>
+            <div class="view-detail"><a href="#">Xem thêm...</a></div>
+        </div>
+        <hr>
+        <div class="news-box">
+            <div class="title-news-content">Title</div>
+            <img class="img-news" src="images/news.jpg" alt="">
+            <div class="content-right">
+                Một số cử tri đề nghị thành phố tập trung giải quyết những vấn đề liên quan đến ranh giới đất đai giữa địa phương với quốc phòng.
+
+                Tiếp thu ý kiến, Bí thư Hà Nội cho rằng, đây là lĩnh vực mà những việc nhỏ không được giải quyết nhanh và tích cực thì có thể nảy sinh thành vấn đề lớn.
+            </div>
+            <div class="view-detail"><a href="#">Xem thêm...</a></div>
+        </div>
+
+
+    </article>
+    <aside>
+        <div class="box-ad-news">
+            <a href="#"><img src="images/quang cao.jpg" alt=""></a>
+        </div>
+
+        <div class="box-ad-news">
+            <a href="#"><img src="images/quang cao.jpg" alt=""></a>
+        </div>
+
+    </aside>
+    <div class="clear-fix"></div>
+    <div class="page-number">
+        <ul>
+            <li ><a class="btn-prev" href="#"><img src="images/btn-page.jpg" alt=""></a></li>
+            <li><a href="#">1</a></li
+            <li><a href="#">2</a></li
+            <li><a href="#">3</a></li
+            <li><a href="#">4</a></li
+            <li><a href="#">5</a></li
+            <li><a href="#">6</a></li
+            <li><a href="#">7</a></li
+            <li><a href="#">8</a></li
+            <li><a href="#">9</a></li
+            <li><a href="#">10</a></li
+            <li><a class="btn-next" href="#"><img src="images/btn-page.jpg" alt=""></a></li>
+        </ul>
+    </div>
+</section>
+<footer>
+    <div class="box-footer">
+        <h3>Ban Tổ Chức</h3>
+        <a href="#"><img class="img-logo-organize" src="images/cty-sieu-nhan-viet.png" alt=""></a>
+        <a href="#"><img class="img-logo-organize" src="images/cty-mouse.png" alt=""></a>
+    </div>
+    <div class="box-footer logo-footer">
+        <a href="#"><img src="images/logo.png" alt="logo ngai vàng điện ảnh"></a>
+    </div>
+    <div class="box-footer contact ">
+        <div class="contact-footer">
+            Liên hệ BTC <br>
+            0909 000000
+        </div>
+        <div class="contact-footer">
+            Liên hệ quảng áo <br>
+            Công ty Dihaan Media <br>
+            0909 000000
+        </div>
+    </div>
+    <div class="donors"><span> Nhà tài trợ</span>
+        <a href="#" class="logo-dornor"><img src="images/icon-cat.png" alt="Nhà tài trợ"></a>
+        <a href="#" class="logo-dornor"><img src="images/icon-cat.png" alt="Nhà tài trợ"></a>
+    </div>
+    <div class="copyright">
+        <div class="copy">All right reserved Dehaan Media.</div>
+        <div class="copy">Giấy phép số BCT/123445/78</div>
+    </div>
+</footer>
+<script src="js/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script src="js/slick.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/video.js"></script>
+<script src="js/youtube.js"></script>
+<script type="text/javascript">
+    $(document).on('ready', function() {
+        $(".regular").slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            speed: 500
+        });
+        /*js menu top mobile*/
+        $('.btn-menu').click(function(event){
+            event.stopPropagation();
+            $('.menu-top-scroll').fadeIn();
+        });
+        $('.close-x').click(function () {
+            $('.menu-top-scroll').fadeOut();
+        })
+        /*js nav scroll top*/
+        function sticky_relocate() {
+            var window_top = $(window).scrollTop();
+            var div_top = $('#sticky-anchor-scroll').offset().top;
+            if (window_top > div_top) {
+                $('.nav-top').addClass('stick-scroll');
+                $('#sticky-anchor-scroll').height($('.nav-top').outerHeight());
+            } else {
+                $('.nav-top').removeClass('stick-scroll');
+                $('#sticky-anchor-scroll').height(0);
+            }
+        }
+        $(function() {
+            $(window).scroll(sticky_relocate);
+            sticky_relocate();
+        });
+    });
+</script>
+</body>
+</html>
---
+++ thisinh-image.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Thi sinh</title>
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <link href="css/video-js.css" rel="stylesheet">
+    <script src="js/videojs-ie8.min.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<header>
+    <div class="intro-video">
+        <video poster="images/poster.png" id="my-video"
+               class="video-js"
+                controls
+                width="100%" height="264"
+                data-setup='{ "techOrder": ["youtube", "html5"], "sources": [{ "type": "video/youtube", "src": "https://www.youtube.com/watch?v=aajlaOzL2-Q"}] }'
+        >
+            <p class="vjs-no-js">
+                To view this video please enable JavaScript, and consider upgrading to a web browser that
+            </p>
+        </video>
+        <img src="images/logo.png" alt="logo" class="logo-video-nvda">
+    </div>
+    <div id="sticky-anchor-scroll"></div>
+    <nav class="nav-top nav-home">
+        <a href="http://ngaivangdienanh.com">
+            <img src="images/logo.png" alt="logo" class="logo-nav-nvda">
+        </a>
+        <ul class="menu-top-scroll">
+            <li class="menu-detail btn-close"><span class="close-x">X</span></li>
+            <li class="menu-detail">
+                <a href="">Giới thiệu</a>
+            </li>
+            <li class="menu-detail">
+                <a class="active" href="">Thể lệ</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Tin tức</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Hình ảnh</a>
+            </li>
+            <li class="menu-detail">
+                <a href="">Video</a>
+            </li>
+        </ul>
+        <img class="btn-menu" src="images/btn-menu.png" alt="menu">
+    </nav>
+    <article class="title-head">
+        <div class="row-bar"></div>
+        <div class="label-head">Thí Sinh</div>
+        <div class="box-title-photos"></div>
+    </article>
+</header>
+<section class="photo-section">
+    <div class="title-content-photos">Hình Ảnh</div>
+    <article>
+        <div class="box-image-show-candidates">
+            <div class="img-show-candidates"></div>
+            <div class="slider-show-candidates">
+                <div class="box-show-candidates"></div>
+                <div class="box-show-candidates"></div>
+            </div>
+        </div>
+        <div class="content-images">
+            <div class="image-album"></div>
+            <div class="image-album"></div>
+            <div class="image-album"></div>
+            <div class="image-album"></div>
+            <div class="image-album"></div>
+            <div class="image-album"></div>
+            <div class="image-album"></div>
+            <div class="image-album"></div>
+        </div>
+    </article>
+</section>
+<footer>
+    <div class="box-footer">
+        <h3>Ban Tổ Chức</h3>
+        <a href="#"><img class="img-logo-organize" src="images/cty-sieu-nhan-viet.png" alt=""></a>
+        <a href="#"><img class="img-logo-organize" src="images/cty-mouse.png" alt=""></a>
+    </div>
+    <div class="box-footer logo-footer">
+        <a href="#"><img src="images/logo.png" alt="logo ngai vàng điện ảnh"></a>
+    </div>
+    <div class="box-footer contact ">
+        <div class="contact-footer">
+            Liên hệ BTC <br>
+            0909 000000
+        </div>
+        <div class="contact-footer">
+            Liên hệ quảng áo <br>
+            Công ty Dihaan Media <br>
+            0909 000000
+        </div>
+    </div>
+    <div class="donors"><span> Nhà tài trợ</span>
+        <a href="#" class="logo-dornor"><img src="images/icon-cat.png" alt="Nhà tài trợ"></a>
+        <a href="#" class="logo-dornor"><img src="images/icon-cat.png" alt="Nhà tài trợ"></a>
+    </div>
+    <div class="copyright">
+        <div class="copy">All right reserved Dehaan Media.</div>
+        <div class="copy">Giấy phép số BCT/123445/78</div>
+    </div>
+</footer>
+<script src="js/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script src="js/slick.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/video.js"></script>
+<script src="js/youtube.js"></script>
+<script type="text/javascript">
+    $(document).on('ready', function() {
+        $(".regular").slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            speed: 500
+        });
+        /*js menu top mobile*/
+        $('.btn-menu').click(function(event){
+            event.stopPropagation();
+            $('.menu-top-scroll').fadeIn();
+        });
+        $('.close-x').click(function () {
+            $('.menu-top-scroll').fadeOut();
+        })
+        /*js nav scroll top*/
+        function sticky_relocate() {
+            var window_top = $(window).scrollTop();
+            var div_top = $('#sticky-anchor-scroll').offset().top;
+            if (window_top > div_top) {
+                $('.nav-top').addClass('stick-scroll');
+                $('#sticky-anchor-scroll').height($('.nav-top').outerHeight());
+            } else {
+                $('.nav-top').removeClass('stick-scroll');
+                $('#sticky-anchor-scroll').height(0);
+            }
+        }
+        $(function() {
+            $(window).scroll(sticky_relocate);
+            sticky_relocate();
+        });
+    });
+</script>
+</body>
+</html>
