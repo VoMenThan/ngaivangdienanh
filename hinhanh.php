4d2855d0b2240e2b3dae4fb4547470e6c574a1e6
VoMenThan <thancn12@gmail.com>
2017-05-08 10:20:38 +0700

    update

--- hinhanh.php
+++ hinhanh.php
@@ -8,11 +8,11 @@
     <title>hình ảnh</title>
     <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
     <link href="css/video-js.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/photoswipe.css">
-    <link rel="stylesheet" href="css/default-skin/default-skin.css">
+
     <script src="js/videojs-ie8.min.js"></script>
-    <script src="js/photoswipe.js"></script>
-    <script src="js/photoswipe-ui-default.js"></script>
+    <script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+    <script src="js/jssor.slider-23.1.6.mini.js" type="text/javascript"></script>
+    <script src="js/custom_javascript.js" type="text/javascript"></script>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
@@ -65,42 +65,85 @@
     <div class="title-content-photos">Hình Ảnh</div>
     <article>
         <div class="box-image-show">
-            <div class="img-show">
-                <img class="img-big" src="images/slider.jpg" alt="">
+        <div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:800px;height:456px;overflow:hidden;visibility:hidden;">
+
+            <div data-u="loading" style="position:absolute;top:0px;left:0px;background-color:rgba(0,0,0,0.7);">
+                <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+                <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
             </div>
-            <div class="slider-show my-gallery">
-                <figure itemprop="associatedMedia" class="box-show-small" itemscope>
-                    <a href="images/slider.jpg" data-size="1024x600">
-                        <img src="images/slider.jpg" itemprop="thumbnail" alt="Image description" />
-                    </a>
-                </figure>
-                <figure itemprop="associatedMedia" class="box-show-small" itemscope>
-                    <a href="images/slider.jpg" data-size="1024x600">
-                        <img src="images/slider.jpg" itemprop="thumbnail" alt="Image description" />
-                    </a>
-                </figure>
-                <figure itemprop="associatedMedia" class="box-show-small" itemscope>
-                    <a href="images/slider.jpg" data-size="700x1200">
-                        <img src="images/slider.jpg" itemprop="thumbnail" alt="Image description" />
-                    </a>
-                </figure>
-                <figure itemprop="associatedMedia" class="box-show-small" itemscope>
-                    <a href="images/slider.jpg" data-size="1024x600">
-                        <img src="images/slider.jpg" itemprop="thumbnail" alt="Image description" />
-                    </a>
-                </figure>
-                <figure itemprop="associatedMedia" class="box-show-small" itemscope>
-                    <a href="images/slider.jpg" data-size="1024x600">
-                        <img src="images/slider.jpg" itemprop="thumbnail" alt="Image description" />
-                    </a>
-                </figure>
-                <figure itemprop="associatedMedia" class="box-show-small" itemscope>
-                    <a href="images/slider.jpg" data-size="1024x600">
-                        <img src="images/slider.jpg" itemprop="thumbnail" alt="Image description" />
-                    </a>
-                </figure>
+            <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:800px;height:356px;overflow:hidden;">
+                <div>
+                    <img data-u="image" src="images/slider.jpg" />
+                    <img data-u="thumb" src="images/slider.jpg" />
+                </div>
+                <div>
+                    <img data-u="image" src="images/slider.jpg" />
+                    <img data-u="thumb" src="images/slider.jpg" />
+                </div>
+                <div>
+                    <img data-u="image" src="images/slider.jpg" />
+                    <img data-u="thumb" src="images/slider.jpg" />
+                </div>
+                <div>
+                    <img data-u="image" src="img/04.jpg" />
+                    <img data-u="thumb" src="img/thumb-04.jpg" />
+                </div>
+                <div>
+                    <img data-u="image" src="img/05.jpg" />
+                    <img data-u="thumb" src="img/thumb-05.jpg" />
+                </div>
+                <div>
+                    <img data-u="image" src="img/06.jpg" />
+                    <img data-u="thumb" src="img/thumb-06.jpg" />
+                </div>
+                <div>
+                    <img data-u="image" src="img/07.jpg" />
+                    <img data-u="thumb" src="img/thumb-07.jpg" />
+                </div>
+                <div>
+                    <img data-u="image" src="img/08.jpg" />
+                    <img data-u="thumb" src="img/thumb-08.jpg" />
+                </div>
+                <div>
+                <img data-u="image" src="img/09.jpg" />
+                <img data-u="thumb" src="img/thumb-09.jpg" />
+                </div>
+                <div>
+                <img data-u="image" src="img/10.jpg" />
+                <img data-u="thumb" src="img/thumb-10.jpg" />
+                </div>
+                <div>
+                <img data-u="image" src="img/11.jpg" />
+                <img data-u="thumb" src="img/thumb-11.jpg" />
+                </div>
+                <div>
+                <img data-u="image" src="img/09.jpg" />
+                <img data-u="thumb" src="img/thumb-09.jpg" />
+                </div>
+                <div>
+                <img data-u="image" src="img/10.jpg" />
+                <img data-u="thumb" src="img/thumb-10.jpg" />
+                </div>
+                <div>
+                <img data-u="image" src="img/11.jpg" />
+                <img data-u="thumb" src="img/thumb-11.jpg" />
+                </div>
 
             </div>
+
+            <div data-u="thumbnavigator" class="jssort01" style="position:absolute;left:0px;bottom:0px;width:800px;height:100px;" data-autocenter="1">
+
+                <div data-u="slides" style="cursor: default;">
+                    <div data-u="prototype" class="p">
+                        <div class="w">
+                            <div data-u="thumbnailtemplate" class="t"></div>
+                        </div>
+                        <div class="c"></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
         </div>
         <div class="title-content-photos">Album</div>
         <div class="content-images">
@@ -211,274 +254,5 @@
     });
 </script>
 
-<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="pswp__bg"></div>
-
-    <!-- Slides wrapper with overflow:hidden. -->
-    <div class="pswp__scroll-wrap">
-
-        <!-- Container that holds slides. PhotoSwipe keeps only 3 slides in DOM to save memory. -->
-        <!-- don't modify these 3 pswp__item elements, data is added later on. -->
-        <div class="pswp__container">
-            <div class="pswp__item"></div>
-            <div class="pswp__item"></div>
-            <div class="pswp__item"></div>
-        </div>
-
-        <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
-        <div class="pswp__ui pswp__ui--hidden">
-
-            <div class="pswp__top-bar">
-
-                <!--  Controls are self-explanatory. Order can be changed. -->
-
-                <div class="pswp__counter"></div>
-
-                <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
-
-                <button class="pswp__button pswp__button--share" title="Share"></button>
-
-                <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-
-                <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-
-                <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
-                <!-- element will get class pswp__preloader--active when preloader is running -->
-                <div class="pswp__preloader">
-                    <div class="pswp__preloader__icn">
-                        <div class="pswp__preloader__cut">
-                            <div class="pswp__preloader__donut"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                <div class="pswp__share-tooltip"></div>
-            </div>
-
-            <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
-            </button>
-
-            <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
-            </button>
-
-            <div class="pswp__caption">
-                <div class="pswp__caption__center"></div>
-            </div>
-
-        </div>
-
-    </div>
-
-</div>
-<script>
-    var initPhotoSwipeFromDOM = function(gallerySelector) {
-
-        // parse slide data (url, title, size ...) from DOM elements
-        // (children of gallerySelector)
-        var parseThumbnailElements = function(el) {
-            var thumbElements = el.childNodes,
-                numNodes = thumbElements.length,
-                items = [],
-                figureEl,
-                linkEl,
-                size,
-                item;
-
-            for(var i = 0; i < numNodes; i++) {
-
-                figureEl = thumbElements[i]; // <figure> element
-
-                // include only element nodes
-                if(figureEl.nodeType !== 1) {
-                    continue;
-                }
-
-                linkEl = figureEl.children[0]; // <a> element
-
-                size = linkEl.getAttribute('data-size').split('x');
-
-                // create slide object
-                item = {
-                    src: linkEl.getAttribute('href'),
-                    w: parseInt(size[0], 10),
-                    h: parseInt(size[1], 10)
-                };
-
-
-
-                if(figureEl.children.length > 1) {
-                    // <figcaption> content
-                    item.title = figureEl.children[1].innerHTML;
-                }
-
-                if(linkEl.children.length > 0) {
-                    // <img> thumbnail element, retrieving thumbnail url
-                    item.msrc = linkEl.children[0].getAttribute('src');
-                }
-
-                item.el = figureEl; // save link to element for getThumbBoundsFn
-                items.push(item);
-            }
-
-            return items;
-        };
-
-        // find nearest parent element
-        var closest = function closest(el, fn) {
-            return el && ( fn(el) ? el : closest(el.parentNode, fn) );
-        };
-
-        // triggers when user clicks on thumbnail
-        var onThumbnailsClick = function(e) {
-            e = e || window.event;
-            e.preventDefault ? e.preventDefault() : e.returnValue = false;
-
-            var eTarget = e.target || e.srcElement;
-
-            // find root element of slide
-            var clickedListItem = closest(eTarget, function(el) {
-                return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
-            });
-
-            if(!clickedListItem) {
-                return;
-            }
-
-            // find index of clicked item by looping through all child nodes
-            // alternatively, you may define index via data- attribute
-            var clickedGallery = clickedListItem.parentNode,
-                childNodes = clickedListItem.parentNode.childNodes,
-                numChildNodes = childNodes.length,
-                nodeIndex = 0,
-                index;
-
-            for (var i = 0; i < numChildNodes; i++) {
-                if(childNodes[i].nodeType !== 1) {
-                    continue;
-                }
-
-                if(childNodes[i] === clickedListItem) {
-                    index = nodeIndex;
-                    break;
-                }
-                nodeIndex++;
-            }
-
-
-
-            if(index >= 0) {
-                // open PhotoSwipe if valid index found
-                openPhotoSwipe( index, clickedGallery );
-            }
-            return false;
-        };
-
-        // parse picture index and gallery index from URL (#&pid=1&gid=2)
-        var photoswipeParseHash = function() {
-            var hash = window.location.hash.substring(1),
-                params = {};
-
-            if(hash.length < 5) {
-                return params;
-            }
-
-            var vars = hash.split('&');
-            for (var i = 0; i < vars.length; i++) {
-                if(!vars[i]) {
-                    continue;
-                }
-                var pair = vars[i].split('=');
-                if(pair.length < 2) {
-                    continue;
-                }
-                params[pair[0]] = pair[1];
-            }
-
-            if(params.gid) {
-                params.gid = parseInt(params.gid, 10);
-            }
-
-            return params;
-        };
-
-        var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
-            var pswpElement = document.querySelectorAll('.pswp')[0],
-                gallery,
-                options,
-                items;
-
-            items = parseThumbnailElements(galleryElement);
-
-            // define options (if needed)
-            options = {
-
-                // define gallery index (for URL)
-                galleryUID: galleryElement.getAttribute('data-pswp-uid'),
-
-                getThumbBoundsFn: function(index) {
-                    // See Options -> getThumbBoundsFn section of documentation for more info
-                    var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
-                        pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
-                        rect = thumbnail.getBoundingClientRect();
-
-                    return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
-                }
-
-            };
-
-            // PhotoSwipe opened from URL
-            if(fromURL) {
-                if(options.galleryPIDs) {
-                    // parse real index when custom PIDs are used
-                    // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
-                    for(var j = 0; j < items.length; j++) {
-                        if(items[j].pid == index) {
-                            options.index = j;
-                            break;
-                        }
-                    }
-                } else {
-                    // in URL indexes start from 1
-                    options.index = parseInt(index, 10) - 1;
-                }
-            } else {
-                options.index = parseInt(index, 10);
-            }
-
-            // exit if index not found
-            if( isNaN(options.index) ) {
-                return;
-            }
-
-            if(disableAnimation) {
-                options.showAnimationDuration = 0;
-            }
-
-            // Pass data to PhotoSwipe and initialize it
-            gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-            gallery.init();
-        };
-
-        // loop through all gallery elements and bind events
-        var galleryElements = document.querySelectorAll( gallerySelector );
-
-        for(var i = 0, l = galleryElements.length; i < l; i++) {
-            galleryElements[i].setAttribute('data-pswp-uid', i+1);
-            galleryElements[i].onclick = onThumbnailsClick;
-        }
-
-        // Parse URL and open gallery if it contains #&pid=3&gid=1
-        var hashData = photoswipeParseHash();
-        if(hashData.pid && hashData.gid) {
-            openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
-        }
-    };
-
-    // execute above function
-    initPhotoSwipeFromDOM('.my-gallery');
-
-</script>
 </body>
 </html>
